af06c7dcdc81b1ce44236b526dbf11ea234322c6
Luis Boscan <luife642@gmail.com>
2017-04-21 10:48:46 -0400

    feat(todos.routes): app routes handled properly

--- apps/todos/routes.js
+++ apps/todos/routes.js
@@ -1,23 +1,81 @@
 var db = require('mongoose'),
-    Todo = db.model('Todo');
+  Todo = db.model('Todo');
 
-module.exports = (function(app) {
+module.exports = (function (app) {
   app.route('/todos')
-    .post(function(req, res) {
-
+    .post(function (req, res) {
+      var todo = new Todo();
+      todo.title = req.body.title;
+      todo.save(function (e) {
+        if (e)
+          res.status(500).send(e);
+      }).then(function () {
+        res.send(todo);
+      });
     })
-    .get(function(req, res) {
-
+    .get(function (req, res) {
+      Todo.find({}, null, { sort: { _id: -1 } }, function (e, data) {
+        if (e) {
+          res.status(500).send(e);
+          return;
+        }
+        res.send(data);
+      });
     });
 
   app.route('/todos/:id')
-    .get(function(req, res) {
-
+    .get(function (req, res) {
+      Todo.findOne({ '_id': req.params.id }, function (e, data) {
+        if (e) {
+          res.status(500).send(e);
+          return;
+        }
+        if (!data) {
+          res.sendStatus(404);
+          return;
+        }
+        res.send(data);
+      });
     })
-    .put(function(req, res) {
+    .put(function (req, res) {
+      Todo.findOne({ '_id': req.params.id }, function (e, data) {
+        if (e) {
+          res.status(500).send(e);
+          return;
+        }
+        if (!data) {
+          res.sendStatus(404);
+          return;
+        }
 
+        data.title = req.body.title || data.title;
+        data.done = req.body.done || data.done;
+
+        data.save(function (error) {
+          if (error) {
+            res.status(500).send(error);
+          }
+        }).then(function () {
+          res.send(data);
+        });
+      });
     })
-    .delete(function(req, res) {
-
+    .delete(function (req, res) {
+      Todo.findOne({ '_id': req.params.id }, function (e, data) {
+        if (e) {
+          res.status(500).send(e);
+        }
+        if (!data) {
+          res.sendStatus(404);
+          return;
+        }
+        data.remove(function (error) {
+          if (error) {
+            res.status(500).send(error);
+            return;
+          }
+          res.sendStatus(200);
+        });
+      });
     });
 });
